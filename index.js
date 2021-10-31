2f833f4f6356ff0ac4a49573ddd83cdc4c6a5afc
ShenQi1996 <qisam1989@gmail.com>
2021-10-31 16:56:20 -0400

    done

--- index.js
+++ index.js
@@ -1,23 +1,76 @@
-const BASE_URL = "https://api.adelphi.edu/v1/departments/?format=json";
+window.addEventListener("load", () => {
+  const BASE_URL = "https://api.adelphi.edu/v1/departments/?format=json";
+  let list1 = document.querySelector(".list");
+  let total = document.querySelector(".total");
+  let count = 0;
+  const CallAPI = async Link => {
+    try {
+      const response = await axios({
+        method: "GET",
+        url: Link,
+        headers: { "Access-Control-Allow-Origin": "*" },
+      });
 
-const getBtn = document.getElementById("get-btn");
+      const results = response.data.results;
+      const Parent = [];
+      results.map(ele => {
+        if (ele.parent == null) {
+          Parent.push(ele);
+          count += 1;
+          let listItem = document.createElement("li");
+          let listItemID = document.createElement("div");
+          let listItemActive = document.createElement("div");
+          let listItemCode = document.createElement("div");
+          let listItemEmail = document.createElement("div");
+          let listItemParent = document.createElement("div");
+          let listItemPlace = document.createElement("div");
+          let listItemTitle = document.createElement("div");
+          let listItemPhone = document.createElement("div");
+          const id = ele.id;
+          const active = ele.active;
+          const code = ele.code;
+          const email = ele.email;
+          const parent = ele.parent;
+          const place = ele.place;
+          const title = ele.title;
+          const phone = ele.phone;
 
-const getParent = async () => {
-  try {
-    const response = await axios({
-      method: "GET",
-      url: `${BASE_URL}`,
-      headers: { "Access-Control-Allow-Origin": "*" },
-    });
+          listItemID.append(`ID: ${id}`);
+          listItem.append(listItemID);
 
-    const Parent = response.data;
+          listItemActive.append(`Active: ${active}`);
+          listItem.append(listItemActive);
 
-    console.log(`GET: Here's the list of Objects`, Parent);
+          listItemCode.append(`Code: ${code}`);
+          listItem.append(listItemCode);
 
-    return Parent;
-  } catch (errors) {
-    console.error(errors);
-  }
-};
+          listItemEmail.append(`Email: ${email}`);
+          listItem.append(listItemEmail);
 
-getBtn.addEventListener("click", getParent);
+          listItemParent.append(`Parent: ${parent}`);
+          listItem.append(listItemParent);
+
+          listItemPlace.append(`Place: ${place}`);
+          listItem.append(listItemPlace);
+
+          listItemTitle.append(`Title: ${title}`);
+          listItem.append(listItemTitle);
+
+          listItemPhone.append(`Phone: ${phone}`);
+          listItem.append(listItemPhone);
+
+          list1.append(listItem);
+        }
+      });
+
+      if (response.data.next != null) CallAPI(response.data.next);
+
+      total.innerHTML = `Number of results: ${count}`;
+      return Parent;
+    } catch (errors) {
+      console.error(errors);
+    }
+  };
+
+  CallAPI(BASE_URL);
+});
